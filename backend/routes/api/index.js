1b41f54236f0d50be12813c9d4ea543ebd09a8b2
gabesauce <alfaro.gabe@gmail.com>
2025-08-08 15:12:39 -0700

    api routes update

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const express = require('express');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser, setTokenCookie, requireAuth } = require("../../utils/auth.js");
