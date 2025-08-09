3aa7b482de3f4f19c9132d4eb262fee1c7669ed8
gabesauce <alfaro.gabe@gmail.com>
2025-08-08 21:10:40 -0700

    add Attachment model, migration, and seed

---
+++ backend/db/migrations/20250809034512-create-attachment.js
@@ -0,0 +1,37 @@
+'use strict';
+// /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Attachments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      applicationId: {
+        type: Sequelize.INTEGER
+      },
+      fileUrl: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Attachments');
+  }
+};
