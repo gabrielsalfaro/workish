059fd0d4952fee6ee6db6d2f259f9bcba91e6668
gabesauce <alfaro.gabe@gmail.com>
2025-08-11 23:38:13 -0700

    Nav, LoginModal, SignupModal, OpenModalButton, context, App, Main update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
-import JobListingsCreate from './components/JobListingsCreate';
+import JobListingsCreate from './components/JobListingsCreate/JobListingsCreate';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -34,17 +32,9 @@ const router = createBrowserRouter([
         element: <h1>Welcome!</h1>
       },
       {
-        path: "login",
-        element: <LoginFormPage />
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />
-      },
-      {
-        path: "/jobs/new",
+        path: '/jobs/new',
         element: <JobListingsCreate />
-      },
+      }
     ]
   }
 ]);
