6997a58443e26755cc79c22f23f31e5ba493e2af
gabesauce <alfaro.gabe@gmail.com>
2025-09-18 23:41:46 -0700

    MyCompany update

--- frontend/src/components/MyCompany/MyCompany.jsx
+++ frontend/src/components/MyCompany/MyCompany.jsx
@@ -1,22 +1,24 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, 
+  // useMemo, useState 
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMyCompany, removeCompany } from '../../store/companies'
 import './MyCompany.css'
 
 const MyCompany = () => {
     const dispatch = useDispatch();
-
+    // const [companyName, setCompanyName] = useState();
     const companyState = useSelector((state) => state.companies);
 //   console.log(companyState)
 
-    const myCompany = useMemo(() => Object.values(companyState)[0], [companyState]);
+    const myCompany = Object.values(companyState)[0] || null;
     // console.log('>>> ', myCompany)
     // console.log('>>> ', myCompany.name)
     useEffect(() => {
         dispatch(fetchMyCompany());
     }, [dispatch]);
 
-    if (!myCompany) return <p>Loading...</p>;
+    // if (!myCompany) return <p>No company assigned.</p>;
 
     const handleCompanyDelete = (companyId) => {
         const confirmDelete = window.confirm('Are you sure you want to delete the company?')
@@ -25,21 +27,36 @@ const MyCompany = () => {
         }
     }
 
+    const handleCompanyAdd = () => {
+      console.log('clicked')
+    }
+
   return (
     <>
-    <div>MyCompany</div>
-    <h2>{myCompany.name}</h2>
-      <p>{myCompany.city}, {myCompany.state}</p>
-      <p>Email: {myCompany.email}</p>
-      <p>Phone: {myCompany.phone}</p>
-      <p>Website: {myCompany.website}</p>
-      {/* <p>Logo: {company.logo}</p> */}
-      <img src={myCompany.logo} alt={`${myCompany.name} logo`} />
-      <button 
-        className="delete-company-button" 
-        onClick={() => handleCompanyDelete(myCompany.id)}
-      >delete
-      </button>
+    { myCompany ? (
+      <>
+        <div>MyCompany</div>
+        <h2>{myCompany?.name}</h2>
+        <p>{myCompany?.city}, {myCompany?.state}</p>
+        <p>Email: {myCompany?.email || 'N/A'}</p>
+        <p>Phone: {myCompany?.phone || 'N/A'}</p>
+        <p>Website: {myCompany?.website || 'N/A'}</p>
+        {/* <p>Logo: {company.logo}</p> */}
+        <img src={myCompany?.logo} alt={`${myCompany?.name} logo`} />
+        <button 
+          className="delete-company-button" 
+          onClick={() => handleCompanyDelete(myCompany?.id)}
+        >delete
+        </button>
+      </>
+    ) : (
+      <>
+      <p>You don&apos;t have a company assigned yet. Search for a company below:</p>
+      <input type="text" className="company-search" />
+      <button className="add-company" onClick={handleCompanyAdd}>Add Company</button>
+      </>
+    )}
+    
     </>
   )
 }
